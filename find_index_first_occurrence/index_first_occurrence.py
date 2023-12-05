18ca77da4357c75594ca6f7e61d14280aecffc32
YurySubbota <plus375291421728@gmail.com>
2023-12-05 18:02:25 +0300

    it's work

--- find_index_first_occurrence/index_first_occurrence.py
+++ find_index_first_occurrence/index_first_occurrence.py
@@ -1,17 +1,12 @@
-haystack = '1sadbutsad'
+haystack = 'sadbutsad'
 needle = 'sad'
 
 
 def finde_the_index_of_the_first_occurrence_in_a_string(string1, string2):
-    if string2 not in string1:
-        return "not"
     for i in range(len(string1)):
-        print(string1[i:len(string1)])
-        print()
         if string2 in string1[i:len(string2)+i]:
-            return i-1
-
+            return i
+    return -1
 
 
 print(finde_the_index_of_the_first_occurrence_in_a_string(haystack, needle))
-#finde_the_index_of_the_first_occurrence_in_a_string(haystack, needle)
