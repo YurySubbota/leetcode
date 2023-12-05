71941924a87ce328712c2302cc14142cd7c12231
YurySubbota <plus375291421728@gmail.com>
2023-12-05 19:20:27 +0300

    it's work

--- search_insert_position/insert_position.py
+++ search_insert_position/insert_position.py
@@ -1,1 +1,13 @@
-...
+nums = [1, 3, 5, 6]
+target = 4
+
+
+def search_position(nums, target):
+    for i in range(len(nums)):
+        if nums[i - 1] < target < nums[i] and i != 0 or target == nums[i] or i == 0 and target < nums[i]:
+            return i
+        elif target > nums[len(nums) - 1]:
+            return len(nums)
+
+
+print(search_position(nums, target))
